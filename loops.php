32c21d82127b54766a2d30552816ad69c9d7fd0a
peterbata <peterbata@hotmail.com>
2017-12-21 11:22:50 -0500

    Loops

---
+++ loops.php
@@ -0,0 +1,22 @@
+<?php
+
+	# Loops - Execute code set number of time_sleep_until
+
+	/*
+		For
+		While
+		Do..While
+		Foreach
+	*/
+
+	# For Loop
+	//@params - init, condition, inc	
+
+	for($i = 0;$i <= 10;$i++){
+
+		echo $i;
+		echo '<br>';
+
+	}
+
+?>
