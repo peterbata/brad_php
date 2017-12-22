fe97090934502545972db47e26b5b54fd242275d
peterbata <peterbata@hotmail.com>
2017-12-21 22:05:13 -0500

    Conditional End

--- conditionals.php
+++ conditionals.php
@@ -13,11 +13,30 @@
 		!==
 	*/
 
-	$num = 5;
-	if($num == 5){
-		echo '5 passed';
+	$num = 6;
+	if($num > 4 XOR $num <10){
+		echo "$num passed";
 	}
 
+	# SWITCH
 
+	$favcolor = 'yellow';
+
+	switch($favcolor){
+		case 'red':
+			echo 'Your favorite color is red';
+		break;
+	
+		case 'blue':
+			echo 'Your favorite color is blue';
+		break;
+		
+		case 'green':
+			echo 'Your favorite color is green';
+		break;
+		
+		default:
+			echo 'Your favorite color is something else';
+}
 
 ?>
