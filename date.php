2150e5a945cca4c61af2cde122bd75ce9e0bdf0e
peterbata <peterbata@hotmail.com>
2017-12-22 19:46:33 -0500

    Episode 8 end

---
+++ date.php
@@ -0,0 +1,17 @@
+<?php
+
+	// echo date('Y/m/d');
+
+	//echo date('H');
+	//echo date('i');
+	
+	
+	date_default_timezone_set('America/Montreal');
+	//echo date('h:i:sa');
+
+	$timestamp = mktime(10, 15, 54, 4, 18, 1957);
+
+	//echo date('m/d/Y h:i:sa', $timestamp);
+
+	
+?>
